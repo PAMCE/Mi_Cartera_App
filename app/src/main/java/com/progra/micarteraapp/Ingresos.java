fbbd832d4ac198fe56cc4f1d8d86e88381878583
pablo <remagforce@gmail.com>
2020-10-20 09:15:15 -0600

    añadi la segunda activity

--- app/src/main/java/com/progra/micarteraapp/Ingresos.java
+++ app/src/main/java/com/progra/micarteraapp/Ingresos.java
@@ -2,19 +2,13 @@ package com.progra.micarteraapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Ingresos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    public void BtnIngresos(){
-            
+        setContentView(R.layout.activity_ingresos);
     }
 }
