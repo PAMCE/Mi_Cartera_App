8f18c47739598b6952704dd1952265712350bd17
pablo <remagforce@gmail.com>
2020-10-20 09:08:59 -0600

    la app aun no tiene logica alguna

---
+++ app/src/main/java/com/progra/micarteraapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.progra.micarteraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void BtnIngresos(){
+
+    }
+}
