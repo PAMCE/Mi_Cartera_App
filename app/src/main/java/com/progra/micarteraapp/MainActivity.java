e8b9ea980a710901f913299584776bbcd34b73df
pablo <remagforce@gmail.com>
2020-10-20 09:19:45 -0600

    se agrego la comunicacion con la segunda activity

--- app/src/main/java/com/progra/micarteraapp/MainActivity.java
+++ app/src/main/java/com/progra/micarteraapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void BtnIngresos(){
-            
+    public void BtnIngresos(View view){
+            Intent Ingresos = new Intent(this, com.progra.micarteraapp.Ingresos.class);
+            startActivity(Ingresos);
     }
 }
