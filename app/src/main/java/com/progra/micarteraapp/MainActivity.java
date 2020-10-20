e16afa6fc03f7e6ed32635c880203b05ac9794dd
pablo <remagforce@gmail.com>
2020-10-20 09:23:56 -0600

    se agrego la comunicacion con la tercera activity

--- app/src/main/java/com/progra/micarteraapp/MainActivity.java
+++ app/src/main/java/com/progra/micarteraapp/MainActivity.java
@@ -15,8 +15,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
+
+    //botones para ir a las otras activitys :v
     public void BtnIngresos(View view){
             Intent Ingresos = new Intent(this, com.progra.micarteraapp.Ingresos.class);
             startActivity(Ingresos);
     }
+
+    public void BtnGastos (View view){
+        Intent Gastos = new Intent(this, com.progra.micarteraapp.Gastos.class);
+        startActivity(Gastos);
+    }
+
+
 }
