84d37bddf253b88fc561e7124af1e10947b792e9
pablo <remagforce@gmail.com>
2020-10-20 21:56:59 -0600

    agregue el spinner al activity de ingresos

--- app/src/main/java/com/progra/micarteraapp/Ingresos.java
+++ app/src/main/java/com/progra/micarteraapp/Ingresos.java
@@ -3,12 +3,28 @@ package com.progra.micarteraapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Ingresos extends AppCompatActivity {
+
+    private Spinner  spinner1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingresos);
+
+        spinner1 = (Spinner)findViewById(R.id.spinner);
+
+        String [] categorias ={"Ingresos activos","Ingresos pasivos"};
+
+        ArrayAdapter <String> adapter = new ArrayAdapter<>(this, R.layout.spinner_item_ingresos, categorias);
+        spinner1.setAdapter(adapter);
+
     }
+
+
+
 }
